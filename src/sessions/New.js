e5196aa3cbbb9b3b395ee3c430d5758ab68b6152
mdnv <mdnvng@gmail.com>
2020-04-10 18:44:41 -0400

    convert login to functional component

--- src/sessions/New.js
+++ src/sessions/New.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 // import {Redirect} from 'react-router-dom'
 import axios from "axios"
 import {
   NavLink,
+  useHistory,
 } from "react-router-dom"
 import flashMessage from '../shared/flashMessages'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../redux'
 
-class New extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      remember_me: "1",
-      flash: []
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+const New = ({ userData, fetchUsers }) => {
+  let history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberme] = useState('1');
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
+  const handleEmailInput = e => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordInput = e => {
+    setPassword(e.target.value);
+  };
+  const handleRememberMeInput = e => {
+    setRememberme(e.target.value);
+  };
 
-  handleSubmit(event, props) {
-    const { email, password, remember_me } = this.state;
-
+  const handleSubmit = (e) => {
     axios
       .post(
         "http://localhost:3000/login",
@@ -37,7 +33,7 @@ class New extends Component {
           session: {
             email: email,
             password: password,
-            remember_me: remember_me
+            remember_me: rememberMe
           }
         },
         { withCredentials: true }
@@ -45,7 +41,7 @@ class New extends Component {
       .then(response => {
         if (response.data.user) {
           fetchUsers();
-          this.props.history.push("/");
+          history.push("/");
         }
         if (response.data.flash) {
           flashMessage(...response.data.flash)
@@ -54,67 +50,65 @@ class New extends Component {
       .catch(error => {
         console.log(error)
       });
-    event.preventDefault();
+    e.preventDefault();
   }
 
-  render() {
-    return (
-      <React.Fragment>
-      {/* <div className={"alert alert-" + this.state.flash[0]}>{this.state.flash[1]}</div> */}
-      <h1>Log in</h1>
-      <div className="row">
-        <div className="col-md-6 col-md-offset-3">
-          <form
-          action="/login"
-          acceptCharset="UTF-8"
-          method="post"
-          onSubmit={this.handleSubmit}
-          >
+  return (
+    <React.Fragment>
+    {/* <div className={"alert alert-" + this.state.flash[0]}>{this.state.flash[1]}</div> */}
+    <h1>Log in</h1>
+    <div className="row">
+      <div className="col-md-6 col-md-offset-3">
+        <form
+        action="/login"
+        acceptCharset="UTF-8"
+        method="post"
+        onSubmit={handleSubmit}
+        >
 
-            <label htmlFor="session_email">Email</label>
+          <label htmlFor="session_email">Email</label>
+          <input
+          className="form-control"
+          type="email"
+          name="email"
+          id="session_email"
+          value={email}
+          onChange={handleEmailInput}
+          />
+
+          <label htmlFor="session_password">Password</label>
+          <a href="/password_resets/new">(forgot password)</a>
+          <input
+          className="form-control"
+          type="password"
+          name="password"
+          id="session_password"
+          value={password}
+          onChange={handlePasswordInput}
+          />
+
+          <label className="checkbox inline" htmlFor="session_remember_me">
             <input
-            className="form-control"
-            type="email"
-            name="email"
-            id="session_email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            />
-
-            <label htmlFor="session_password">Password</label>
-            <a href="/password_resets/new">(forgot password)</a>
+            name="remember_me"
+            type="hidden"
+            value="0" />
             <input
-            className="form-control"
-            type="password"
-            name="password"
-            id="session_password"
-            value={this.state.password}
-            onChange={this.handleChange}
+            type="checkbox"
+            checked="checked"
+            name="remember_me"
+            id="session_remember_me"
+            value={rememberMe}
+            onChange={handleRememberMeInput}
             />
-
-            <label className="checkbox inline" htmlFor="session_remember_me">
-              <input
-              name="remember_me"
-              type="hidden"
-              value="0" />
-              <input
-              type="checkbox"
-              checked="checked"
-              name="remember_me"
-              id="session_remember_me"
-              value={this.state.remember_me}
-              onChange={this.handleChange}
-              />
-              <span>Remember me on this computer</span>
-            </label>
-            <input type="submit" name="commit" value="Log in" className="btn btn-primary" data-disable-with="Log in" />
-      </form>
-          <p>New user? <NavLink to="/signup">Sign up now!</NavLink></p>
-        </div>
+            <span>Remember me on this computer</span>
+          </label>
+          <input type="submit" name="commit" value="Log in" className="btn btn-primary" data-disable-with="Log in" />
+        </form>
+        <p>New user? <NavLink to="/signup">Sign up now!</NavLink></p>
       </div>
-      </React.Fragment>
-    );
-  }
+    </div>
+    </React.Fragment>
+  );
 }
 
 const mapStateToProps = state => {
